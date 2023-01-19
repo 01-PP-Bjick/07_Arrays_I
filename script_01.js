7093bc32c9566f30f8bf8c90f45558a4197f5596
bjick84 <bjick@web.de>
2023-01-19 14:52:03 +0100

    01b. Funtionalität mit Array 2

--- script_01.js
+++ script_01.js
@@ -34,10 +34,61 @@
 //     output("index i: " + i);
 // }
 
-// Inkrement (var. Schrittweite)
-for (let i = 0; i < 100; i+=10) {
-    output("index i: " + i);   
-}
+// // Inkrement (var. Schrittweite)
+// for (let i = 0; i < 100; i+=5) {
+//     output("index i: " + i);   
+// }
+
+/* 02b. For-Schleife für Array-Index (Iteration)*/
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++) {
+//     output("index i:" + i + " " + arr[i]); // i als Index des Arrays
+// }
+
+ /*********   Überlegungen - Transponierung **********/
+
+
+
+/*
+1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
+Solange die Variable existiert, bleibt dieser erhalten.
+hier: Aufsummierung
+*/
+
+/*
+1a. Einer Variablen kann Ihr eigener Wert zugewiesen werden
+Solange die Variable existiert, bleibt dieser erhalten
+hier: Verkettung eines Strings // Transponierung
+*/
+
+// let str = "";
+// let gap = " ";
+// let addStr = "Test";
+// for (let i = 0; i <= 5; i++) {
+//     output("in der loop: " + str);
+//     // a = a + 1;  // Numerik
+//     str = str + addStr + gap // Text
+// }
+// output("nach der loop: " + str);
+
+/* Zu "Fuß" - DRY !!! */
+// let a = 0; // Anfangswert
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 0 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 1 + 1
+// output("inhalt von a: " + a);
+// a = a + 1;  // a = 2 + 1
+// output("inhalt von a: " + a);
+
+/* Besser: mit FOR-Schleife */
+// let a = 0; // Anfangswert
+// for (let i = 0; i < 10; i++) {
+//     output (i);
+//     output("in der loop vor +:" + a);
+//     a = a + 1;
+//     output("in der loop nach +:" + a);
+// }
 
 /*** 01. Funktionalität mit Einzelparametern */
 // --> "Ich bin Max Mütze."
@@ -75,6 +126,26 @@ function getSentenceArr(arr) {
                 PUNCT;
     return str;
 }
+
+/*** 01b. Funtionalität mit Array 2 ***/
+//Transponierung: untereinander ---> nebeneinander
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]))
+output(getSentenceArr2(["Ich","bin","die","coole"]))
+output(getSentenceArr2(["Ich","bin"]))
+function getSentenceArr2(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        str = str + arr[i] + GAP;
+        // output(str);
+        }
+    
+    str = str + PUNCT
+    return str;
+}
+
 
 // Modul: Ausgabe in Konsole : Test
 // output("hi"); 
